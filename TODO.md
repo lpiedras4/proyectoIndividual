43c064f7885c7eea3bd7a384ce9b479170c5953d
lpiedras4 <piedras.leo@gmail.com>
2026-04-22 15:50:00 -0700

    se agrego funcionaidad al grid expandible

---
+++ TODO.md
@@ -0,0 +1,5 @@
+TODO
+- Crear diseño responsivo.
+- Agregar links en universidades.
+- Terminar documento.
+- Arreglar h2 de campoLaboral.html
--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+document.querySelectorAll('.accordion-header').forEach(button =>{
+  button.addEventListener('click', ()=>{
+    const item = button.parentElement;
+    const icon = button.querySelector('.icon');
+    const isOpen = item.classList.toggle('open');
+    icon.textContent = isOpen ? '∧' : '∨';
+  });
+  });
