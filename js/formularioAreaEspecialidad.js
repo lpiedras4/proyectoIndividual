f029502fbc5e3705d5f8506b7848120b6cf0e201
lpiedras4 <piedras.leo@gmail.com>
2026-05-05 16:12:15 -0700

    se agregó funcionalidad de generar perfil[

--- js/formularioAreaEspecialidad.js
+++ js/formularioAreaEspecialidad.js
@@ -1,18 +1,41 @@
-  /*Obtener elementos interactivos del DOM*/
-  const areaInteres = document.querySelector("form-aspirante-resultado");
-  const btnGenerarPerfil = document.getElementById("form-aspirante-btn");
-  const selectArea =  document.querySelector("form-aspirante-select");
-  const anadirInteres = document.getElementById("form-aspirante-add");
-  const setIntereses = new Set();
-
-  anadirInteres.addEventListener("click", (e) => {
-   const interes = document.getElementById("form-aspirante-add").value.trim();
-   console.log(interes);
-  })
 
 
-  btnGenerarPerfil.addEventListener("click", (e) => {
-    const textoAreaInteres = selectArea.options[selectArea.selectedIndex].text;
-    console.log(textoAreaInteres);
-    let descripcion
-  });
+/*Obtener elementos interactivos del DOM*/
+const resultado = document.querySelector(".form-aspirante-resultado");
+const btnGenerarPerfil = document.getElementById("form-aspirante-btn");
+const areaSW = document.querySelector(".form-aspirante-select");
+const anadirInteres = document.getElementById("form-aspirante-add");
+const setHabilidades = new Set();
+const enfoques = new Map();
+enfoques.set("Web", "Construcción de aplicaciones web modernas");
+enfoques.set("Data", "Análisis de datos y modelos predictivos.");
+enfoques.set("Mobile", "Desarrollo de apps móviles.");
+
+let enfoque = "";
+let fortalezas = "";
+
+anadirInteres.addEventListener("click", (e) => {
+
+  const habilidad = document.getElementById("form-aspirante-input").value.trim();
+  if (habilidad === "") {
+    alert("Ingresa un interés");
+  }
+  if (setHabilidades.has(habilidad)) {
+    alert("Este interés ya ha sido agregado, ingresa otro");
+  } else {
+    setHabilidades.add(habilidad);
+    console.log(habilidad + " agregada")
+  }
+});
+
+btnGenerarPerfil.addEventListener("click", (e) => {
+  const valor = areaSW.value;
+  enfoque = enfoques.get(valor);
+  resultado.innerHTML = `
+    <h3>Tu Perfil de Futuro Ingeniero: ${valor} </h3>
+      <p><strong>Enfoque:</strong> ${enfoque}</p>
+      
+      <p><strong>Fortalezas:</strong></p>
+      <span class="form-aspirante-tag"></span>
+    `;
+});
